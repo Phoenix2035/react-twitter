436d58fd47901f07974de81a32a60f46cb4074bd
masturdating1991 <aligodosi@gmail.com>
2020-10-08 14:50:56 +0330

    init

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Layout(props) {
+    return (
+        <div>
+            <div>Left</div>
+            <div>Main</div>
+            <div>Right</div>
+        </div>
+    );
+}
+
+export default Layout;
